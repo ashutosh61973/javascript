d516b8c2274c6e4c8ecb550d06763d1becd64731
ashutosh61973 <ashutoshd2001@gmail.com>
2021-06-17 16:18:16 +0530

    again change var

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ return student;
 Student.prototype.getName=function(){
 return student.name;
 };
-var st3= new Student("ashu",19103059,50);
-console.log(st3);
+var st1= new Student("ashu",19103059,50);
+console.log(st1);
 var st2= new Student("ashutosh",19103058,52);
 console.log(st2);
 // console.log(Object.getPrototypeof(st1));
